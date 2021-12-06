db6b2f58c79b8f823d0a8a4db1488d0097289e77
CarlosVelilla <carlosvelillaf@hotmail.com>
2021-12-06 03:33:01 +0100

    Add project structure & Start MVC structure & Add basics

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path')
+
+const routes = require('./routes')
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, './views'));
+
+app.use('/', routes());
+
+app.listen(3000);
